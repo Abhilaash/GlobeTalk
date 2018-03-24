9a62db36ff619944836b28088ab5802de566c5f8
Abhilaash <abhilaash.velamati@gmail.com>
2018-03-24 19:41:55 -0400

    New Activity and fragments

--- app/src/main/java/com/hackuva/globetalk/Login.java
+++ app/src/main/java/com/hackuva/globetalk/Login.java
@@ -2,13 +2,28 @@ package com.hackuva.globetalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.GlobeTalk.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Welcome.class);
+        EditText username = (EditText) findViewById(R.id.input_email);
+        EditText password = (EditText) findViewById((R.id.input_password));
+
+        String user = username.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, user);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/hackuva/globetalk/Welcome.java
@@ -0,0 +1,69 @@
+package com.hackuva.globetalk;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Welcome extends AppCompatActivity {
+    FragmentPagerAdapter adapterViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Login.EXTRA_MESSAGE);
+
+        setTitle("Welcome: " + message);
+
+        ViewPager vpPager = (ViewPager) findViewById(R.id.pager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 2;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show the first fragment
+                    return StudentOrTeacher.newInstance(0);
+                case 1: // Fragment # 1 - This will show the second fragment
+                    return StudentOrTeacher.newInstance(1);
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
+
+    }
+}
+
